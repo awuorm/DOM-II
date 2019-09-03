660813e5470d7fb925292eba398795552033d5e2
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-09-03 14:53:36 +0300

    started on task 2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+//Mouseover event listener
+let navMouseOver = document.querySelectorAll("a");
+navMouseOver.forEach((arr) => arr.addEventListener
+('mouseover',(event) => event.target.style.color = "teal"));
+
+//Keydown event listener
+let bodyKeyDown = document.querySelector("body");
+bodyKeyDown.addEventListener('keydown', (event) => event.target.style.backgroundColor = "antiquewhite");
